e9c7074b432b0f1407d27e6f4baa75667db0131b
lovepreet-singh-code <info.thelovepreetsingh@gmail.com>
2026-01-06 15:54:25 +0530

    feat: add user creation functionality with validation

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import userRoutes from "./routes/user.routes";
+
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api", userRoutes);
 
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "OK", message: "Server is healthy 🚀" });
